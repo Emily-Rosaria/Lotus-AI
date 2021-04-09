c3a56060bd433fdd75fb43a1b28a2e810a10ed5e
Emily Rose <ebonywoodhellscythe@gmail.com>
2021-04-09 18:55:04 +0100

    Improve role command for multiple roles

--- commands/dev/addroles.js
+++ commands/dev/addroles.js
@@ -6,14 +6,35 @@ module.exports = {
     description: 'Reloads all the commands and procedures!', // The description of the command (for help text)
     perms: 'dev', //restricts to bot dev only (me)
     allowDM: false,
-    usage: '<rolename1> <colour1|none> [rolename2] [colour2|none] ...', // Help text to explain how to use the command (if it had any arguments)
+    usage: '<rolename1> <colour1|none>\n[rolename2] [colour2|none]\n[rolename3] [colour3|none]\n...', // Help text to explain how to use the command (if it had any arguments)
     async execute(message, args) {
-      for (i = 0; 2*i < args.length; i++) {
+      var role_args = message.split(/\n/);
+      if (role_args.length == 1) {
+        role_args = message.split(/ +/);
+        role_args.shift(); //remove command trigger
+        var color = role_args.length > 1 ? role_args.pop() : 'none';
+        role_args = [role_args.join(' '), color];
+      } else {
+        role_args[0] = role_args[0].split(/ +/);
+        if (role_args[0].length == 1) {
+          role_args.shift();
+        } else {
+          role_args[0].shift();
+          role_args[0].join(' '); //remove command trigger
+        }
+        role_args.map(row => {
+          var items = row.split(/ +/);
+          var color = items.length > 1 ? items.pop() : 'none';
+          var items = items.join(' ');
+          return [items, color];
+        })
+      }
+      for (i = 0; 2*i < role_args.length; i++) {
         var data = {
-          name: args[2*i].trim()
+          name: role_args[2*i].trim()
         };
-        if (args.length != 2*i + 1 && args[2*i+1] != 'none') {
-          data.color = args[2*i+1].trim();
+        if (role_args.length != 2*i + 1 && role_args[2*i+1] != 'none') {
+          data.color = role_args[2*i+1].trim();
         }
         await message.guild.roles.create({data:data});
       }
