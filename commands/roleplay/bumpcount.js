a6288f89529cc382b40c58b2d7f365c72860cdeb
Emily <ebonywoodhellscythe@gmail.com>
2022-08-27 23:52:11 +0100

    fix

--- commands/roleplay/bumpcount.js
+++ commands/roleplay/bumpcount.js
@@ -29,7 +29,7 @@ module.exports = {
 					interaction.reply({embeds: [embed]});
 				}).catch((e) => {
 					console.error(e);
-					const embed = new Discord.MessageEmbed().setTitle('Unknown User\'s Wordcounts').setFooter({ text:`ID: ${userID}`}).setTimestamp().setDescription(`Total Bumps: \`${docs.bumps}\``);
+					const embed = new Discord.MessageEmbed().setTitle('Unknown User\'s Bump Count').setFooter({ text:`ID: ${userID}`}).setTimestamp().setDescription(`Total Bumps: \`${docs.bumps}\``);
 					interaction.reply({embeds: [embed]});
 				})
 			});
