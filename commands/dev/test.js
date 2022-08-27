c395f55b5cebe06376e610b4e3edd773b8843171
Emily <ebonywoodhellscythe@gmail.com>
2022-08-28 00:31:37 +0100

    done

--- commands/dev/test.js
+++ commands/dev/test.js
@@ -13,6 +13,60 @@ module.exports = {
     perms: 'dev', //restricts to bot dev only (me)
     group: 'dev',
     async execute(message) {
-      console.log(await Users.findById(message.author.id).exec());
+      if (message.author.id != "247344219809775617") {return}
+
+      return message.reply("Write code bitch, yeet.");
+
+      const client = message.client;
+      let guild = await client.guilds.fetch(config.guild);
+      const rpGroups = Object.keys(config.rpChannels);
+      let channel = await guild.channels.fetch('892995501627154450');
+      let userbumps = {};
+      let last = "1013223096477556817";
+      let quit = false;
+      for (let i = 0; i<13 && !quit; i++) {
+        console.log("i = "+i);
+        await channel.messages.fetch({ limit: 100, before: last }).then(async messages => {
+          if (!messages || messages.size == 0) {
+            console.log("No messages.");
+            quit = true;
+            return;
+          }
+          messages.forEach(async (msg,index) => {
+            if (msg.author.id != "302050872383242240") {
+              return;
+            }
+            if (last == msg.id) {
+              if (index == 0) {
+                return;
+              }
+              quit = true;
+              console.log("Consecutive dupes.");
+              return;
+            }
+            if (msg.interaction && msg.interaction.commandName && msg.interaction.commandName == "bump" && msg.interaction.user && msg.interaction.user.id) {
+              last = msg.id;
+              if (userbumps[msg.interaction.user.id]) {
+                userbumps[msg.interaction.user.id] += 1;
+              } else {
+                userbumps[msg.interaction.user.id] = 1;
+              }
+            }
+          });
+        }).catch(e => {
+          console.error(e);
+          return;
+        });
+        console.log(userbumps);
+      }
+      for (const u in userbumps) {
+        Users.findOneAndUpdate({_id: u},{
+          "$inc": {
+            "bumps": userbumps[u]
+          }
+        }, {upsert: true}).exec();
+        console.log("Updated "+u+" with "+userbumps[u]+" bumps!");
+      }
+      console.log("Done!");
     },
 };
